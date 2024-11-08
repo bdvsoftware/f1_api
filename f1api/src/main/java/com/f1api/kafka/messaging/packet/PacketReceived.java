68b560eb39e5bf0458c6bb461dc854aadfe468aa
Borja <bdvazquez17@esei.uvigo.es>
2024-11-08 20:29:20 +0100

    working

--- f1api/src/main/java/com/f1api/kafka/messaging/packet/PacketReceived.java
+++ f1api/src/main/java/com/f1api/kafka/messaging/packet/PacketReceived.java
@@ -1,11 +1,16 @@
 package com.f1api.kafka.messaging.packet;
 
 import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import com.f1api.kafka.messaging.packet.master.*;
+import com.f1api.util.Constants;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,5 +18,9 @@ import com.f1api.kafka.messaging.packet.master.*;
 @Setter
 public class PacketReceived implements Serializable {
     private Short id;
+
+    @JsonSubTypes({
+        @JsonSubTypes.Type(value = ClassA.class, name = Constants.PacketTypes.MOTION_PACKET)
+    })
     private Packet data;
 }
---
+++ f1api/src/main/java/com/f1api/util/Constants.java
@@ -0,0 +1,6 @@
+package com.f1api.util;
+public class Constants {
+    public static final class PacketTypes {
+        public static final String MOTION_PACKET = "m_carMotionData";
+    }
+}
