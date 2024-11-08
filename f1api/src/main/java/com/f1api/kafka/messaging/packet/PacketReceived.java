4439b67ecf7387facced7ac1fe930649f15a3b79
bdvsoftware <bdvsoftware@gmail.com>
2024-11-08 20:05:18 +0100

    need to implement Packet classes

--- f1api/src/main/java/com/f1api/kafka/messaging/packet/PacketReceived.java
+++ f1api/src/main/java/com/f1api/kafka/messaging/packet/PacketReceived.java
@@ -19,8 +19,12 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 public class PacketReceived implements Serializable {
     private Short id;
 
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "packetType")
     @JsonSubTypes({
-        @JsonSubTypes.Type(value = ClassA.class, name = Constants.PacketTypes.MOTION_PACKET)
+        @JsonSubTypes.Type(value = ClassA.class, name = Constants.PacketTypes.MOTION_PACKET),
+        @JsonSubTypes.Type(value = ClassA.class, name = Constants.PacketTypes.LAP_DATA_PACKET),
+        @JsonSubTypes.Type(value = ClassA.class, name = Constants.PacketTypes.CAR_TELEMETRY_PACKET),
+        @JsonSubTypes.Type(value = ClassA.class, name = Constants.PacketTypes.MOTION_EX_PACKET)
     })
     private Packet data;
 }
--- f1api/src/main/java/com/f1api/util/Constants.java
+++ f1api/src/main/java/com/f1api/util/Constants.java
@@ -1,6 +1,9 @@
 package com.f1api.util;
 public class Constants {
     public static final class PacketTypes {
-        public static final String MOTION_PACKET = "m_carMotionData";
+        public static final String MOTION_PACKET = "motionPacketData";
+        public static final String LAP_DATA_PACKET = "lapDataPacketData";
+        public static final String CAR_TELEMETRY_PACKET = "carTelemetryPacketData";
+        public static final String MOTION_EX_PACKET = "motionExPacketData";
     }
 }
