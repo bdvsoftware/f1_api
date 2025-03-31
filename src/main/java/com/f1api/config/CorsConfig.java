b8923c47560e1e15a6f266aa13036f4b243373c7
bdvsoftware <bdvsoftware@gmail.com>
2025-03-31 16:49:00 +0200

    working

--- src/main/java/com/f1api/config/CorsConfig.java
+++ src/main/java/com/f1api/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.f1api.kafka;
+package com.f1api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
