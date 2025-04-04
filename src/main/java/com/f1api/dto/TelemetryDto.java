bd7f3382376b35253d31925382a4cec521d906f6
bdvsoftware <bdvsoftware@gmail.com>
2025-04-04 18:06:04 +0200

    json property

--- src/main/java/com/f1api/dto/TelemetryDto.java
+++ src/main/java/com/f1api/dto/TelemetryDto.java
@@ -3,6 +3,7 @@ package com.f1api.dto;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,14 +12,19 @@ import lombok.Setter;
 @Getter
 @Setter
 public class TelemetryDto<T> {
-    
+
+    @JsonProperty("headerId")
     private UUID headerId;
 
+    @JsonProperty("yAxisName")
     private String yAxisName;
 
+    @JsonProperty("yAxisValue")
     private T yAxisValue;
-    
+
+    @JsonProperty("xAxisValue")
     private BigDecimal xAxisValue;
 
+    @JsonProperty("stintName")
     private String stintName;
 }
